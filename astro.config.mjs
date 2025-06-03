dd3c0282bbb9500116fdba724820a8489017be26
zgt <matthewcalayo@gmail.com>
2025-06-03 17:37:37 -0400

    configure for vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import solid from '@astrojs/solid-js';
-import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
+import { defineConfig } from "astro/config";
+import solid from "@astrojs/solid-js";
+import tailwind from "@astrojs/tailwind";
+import node from "@astrojs/node";
+import vercelServerless from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [solid(), tailwind()],
-  output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  })
+  output: "server",
+  adapter: vercelServerless(),
 });
